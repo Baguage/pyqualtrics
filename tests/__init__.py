7e89b1413ba4bf8bb4f4e0656ddfe2c2b0f919a3
Alexander Vyushkov <baguage@gmail.com>
2016-09-18 12:11:04 -0400

    Added one more SSL Test, updated changelog and contributor list

--- tests/__init__.py
+++ tests/__init__.py
@@ -662,7 +662,7 @@ Use link https://nd.qualtrics.com/jfe/form/SV_8pqqcl4sy2316ZL and answer "Male".
         self.assertIsNone(qualtrics.json_response)
         self.assertIsNone(result)
 
-    def test_ssl_error(self):
+    def test_ssl_error_1(self):
         # This only works on Notre Dame VPN
         qualtrics = Qualtrics(self.user, "123")
         qualtrics.url = "https://vecnet-ingest.crc.nd.edu/"
@@ -672,7 +672,17 @@ Use link https://nd.qualtrics.com/jfe/form/SV_8pqqcl4sy2316ZL and answer "Male".
         result = qualtrics.getLegacyResponseData(SurveyID=self.survey_id)
         self.assertNotIn("CERTIFICATE_VERIFY_FAILED", qualtrics.last_error_message)
 
-        self.assertFalse(True)
+    def test_ssl_error_2(self):
+        # This may fail is 129.74.247.12 is down or certificate error is corrected
+        qualtrics = Qualtrics(self.user, "123")
+        qualtrics.url = "https://129.74.247.12/"
+        result = qualtrics.getLegacyResponseData(SurveyID=self.survey_id)
+        # Error: hostname '129.74.247.12' doesn't match either of '*.vecnet.org', 'vecnet.org'
+        self.assertIn("129.74.247.12", qualtrics.last_error_message)
+        qualtrics.requests_kwargs = {"verify": False}
+        result = qualtrics.getLegacyResponseData(SurveyID=self.survey_id)
+        self.assertNotIn("CERTIFICATE_VERIFY_FAILED", qualtrics.last_error_message)
+
 
     def tearDown(self):
         # Note that tearDown is called after EACH test
