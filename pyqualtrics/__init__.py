dd0da08c0b5084a891854f8d541bc4147ca0d5cb
Simmons, John <JohnSimmons@quickenloans.com>
2017-05-31 13:09:23 -0400

    support for .env file

--- pyqualtrics/__init__.py
+++ pyqualtrics/__init__.py
@@ -30,6 +30,11 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects, HTTP
 
 __version__ = "0.6.0"
 
+if os.path.exists('.env'):
+    for line in open(".env"):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
 
 class Qualtrics(object):
     """
